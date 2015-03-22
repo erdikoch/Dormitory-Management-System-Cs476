c4f5bf30059650222874f8658fdc87258eb23c1e
Erdi <erdi.koc@ozu.edu.tr>
2015-03-22 20:45:11 +0200

    Update CapacityWindow.java

--- DMS/src/View/CapacityWindow.java
+++ DMS/src/View/CapacityWindow.java
@@ -23,7 +23,7 @@ public class CapacityWindow extends ApplicationFrame
    private static PieDataset createDataset( ) 
    {
       DefaultPieDataset dataset = new DefaultPieDataset( );
-      dataset.setValue( "Students" , new Double( 20 ) );  // after student Addititon will be changed
+      dataset.setValue( "Students" , new Double( 20 ) );  
       dataset.setValue( "Employees" , new Double( 20 ) );   
       dataset.setValue( "Teachers" , new Double( 40 ) );    
       dataset.setValue("Dorm Empty Spaces",new Double( 20 ));

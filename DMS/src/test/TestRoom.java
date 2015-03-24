61fd8040efd06cd976333216f5956f974163ff3a
Mehmet Kayaalp <mehmet.kayaalp@ozu.edu.tr>
2015-03-24 19:42:28 +0200

    TestRoom.java updated after Room attributes are changed

--- DMS/src/test/TestRoom.java
+++ DMS/src/test/TestRoom.java
@@ -8,66 +8,113 @@ import background.Room;
 
 public class TestRoom {
 
-//	@Test
-//	public void testTypeName() {
-//		Room room = new Room();
-//		room.setTypeName("vol");
-//		assertEquals("vol", room.getTypeName());
-//	}
-//	
-//	@Test
-//	public void testTypeName2() {
-//		Room room = new Room();
-//		room.setTypeName("");
-//		assertEquals("", room.getTypeName());
-//	}
-//	
-//	@Test
-//	public void testTypeName3() {
-//		Room room = new Room();
-//		room.setTypeName(" ");
-//		assertEquals(" ", room.getTypeName());
-//	}
-//	
-//	@Test
-//	public void testTypeName4() {
-//		Room room = new Room();
-//		room.setTypeName("asdfghj");
-//		assertEquals("asdfghj", room.getTypeName());
-//	}
-//	
 	@Test
-	public void testRoomNo() {
+	public void testZeroTypeName() {
+		Room room = new Room();
+		room.setTypeName(0);
+		assertEquals(0, room.getTypeName());
+	}
+	
+	@Test
+	public void testTwoIntegerTypeName() {
+		Room room = new Room();
+		room.setTypeName(10);
+		assertEquals(10, room.getTypeName());
+	}
+	
+	@Test
+	public void testTypeName() {
+		Room room = new Room();
+		room.setTypeName(5);
+		assertEquals(5, room.getTypeName());
+	}
+	
+	@Test
+	public void testMinusTypeName() {
+		Room room = new Room();
+		room.setTypeName(-1);
+		assertEquals(-1, room.getTypeName());
+	}
+	
+	@Test
+	public void testLongDigitTypeName() {
+		Room room = new Room();
+		room.setTypeName(11111111);
+		assertEquals(11111111, room.getTypeName());
+	}
+	
+	@Test
+	public void testRoomNoZero() {
 		Room room = new Room();
 		room.setRoomNo(0);
 		assertEquals(0, room.getRoomNo());
 	}
 	
 	@Test
-	public void testRoomNo2() {
+	public void testOneDigitRoomNo() {
+		Room room = new Room();
+		room.setRoomNo(2);
+		assertEquals(2, room.getRoomNo());
+	}
+	
+	@Test
+	public void testTwoDigitRoomNo() {
 		Room room = new Room();
 		room.setRoomNo(10);
 		assertEquals(10, room.getRoomNo());
 	}
 	
 	@Test
-	public void testRoomNo3() {
+	public void testThreeDigitRoomNo() {
 		Room room = new Room();
 		room.setRoomNo(100);
 		assertEquals(100, room.getRoomNo());
 	}
+	
 	@Test
-	public void testRoomNo4() {
+	public void testFourDigitRoomNo() {
 		Room room = new Room();
 		room.setRoomNo(1000);
 		assertEquals(1000, room.getRoomNo());
 	}
+	
 	@SuppressWarnings("deprecation")
 	@Test
-	public void testRoomPrice() {
+	public void testOneDigitRoomPrice() {
+		Room room = new Room();
+		room.setRoomPrice((double) 6.0);
+		assertEquals(6.0, room.getRoomPrice());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testTwoDigitRoomPrice() {
+		Room room = new Room();
+		room.setRoomPrice((double) 60.0);
+		assertEquals(60.0, room.getRoomPrice());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testThreeDigitRoomPrice() {
 		Room room = new Room();
 		room.setRoomPrice((double) 600.0);
 		assertEquals(600.0, room.getRoomPrice());
 	}
-
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testLongDigitRoomPrice() {
+		Room room = new Room();
+		room.setRoomPrice((double) 600000.0);
+		assertEquals(600000.0, room.getRoomPrice());
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testAfterPointRoomPrice() {
+		Room room = new Room();
+		room.setRoomPrice((double) 600.3232323);
+		assertEquals(600.3232323, room.getRoomPrice());
+	}
 }

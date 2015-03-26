ffb3088fd05b9c905897d97f07e1ac63afb33bab
Erdi <erdi.koc@ozu.edu.tr>
2015-03-26 16:39:41 +0200

    Update Dorm.java

--- DMS/src/Background/Dorm.java
+++ DMS/src/Background/Dorm.java
@@ -1,7 +1,12 @@
 package background;
 
+import java.util.ArrayList;
+
 
 public class Dorm {
+	
+	private ArrayList<Dorm> dormList;
+	
 	private String dormName;
 	private String location;
 	
@@ -16,6 +21,13 @@ public class Dorm {
 	}
 	public void setLocation(String location) {
 		this.location = location;
+	}
+	public void addDorm(Dorm dorm){
+		dormList.add(dorm);
+	}
+	
+	public int getDormSize(){
+		return dormList.size();
 	}
 	
 }

1dc3be1a754769c9742be90144d57a76f88c2d08
Mehmet Kayaalp <mehmet.kayaalp@ozu.edu.tr>
2015-03-28 16:53:08 +0200

    With test results changed

--- DMS/src/Background/CreditCardPayment.java
+++ DMS/src/Background/CreditCardPayment.java
@@ -2,35 +2,47 @@ package background;
 
 public class CreditCardPayment extends Payment {
 
+	private String cardName;
+	private String expireDate;
+	private String creditCardNum;
 
-	  private String cardName;
-		private String expireDate;
-		private String creditCardNum;
-		
-		public CreditCardPayment(){
-			this.cardName="No card name";
-			this.expireDate="No expire date";
-			this.creditCardNum="No credit card number";
-		}
-
-
-
-		public CreditCardPayment(String cardName, String expireDate, String creditCardNum){
-			this.cardName=cardName;
-			this.expireDate=expireDate;
-			this.creditCardNum=creditCardNum;
-		}
-
-		public void setcardName(String cardName){
-			this.cardName=cardName;
-		}
-
-		public void setexpireDate(String expireDate){
-			this.expireDate=expireDate;
-		}
-
-		public void setcreditCardNum(String creditCardNum){
-			this.creditCardNum=creditCardNum;
-		}
-
+	public CreditCardPayment(){
+		this.cardName="No card name";
+		this.expireDate="No expire date";
+		this.creditCardNum="No credit card number";
 	}
+
+	public CreditCardPayment(String cardName, String expireDate, String creditCardNum){
+		this.cardName=cardName;
+		this.expireDate=expireDate;
+		this.creditCardNum=creditCardNum;
+	}
+
+	public void setCardName(String cardName){
+		if(!cardName.equals("") || !cardName.equals(" ")) {
+			this.cardName=cardName;
+		}
+	}
+	
+	public String getCardName() {
+		return cardName;
+	}
+
+	public String getExpireDate() {
+		return expireDate;
+	}
+
+	public String getCreditCardNum() {
+		return creditCardNum;
+	}
+
+	public void setExpireDate(String expireDate){
+		this.expireDate=expireDate;
+	}
+
+	public void setCreditCardNum(String creditCardNum){
+		if(!creditCardNum.equals("") || !creditCardNum.equals(" ")) {
+			this.creditCardNum=creditCardNum;
+		}
+	}
+}
--- DMS/src/Background/Payment.java
+++ DMS/src/Background/Payment.java
@@ -1,25 +1,29 @@
 package background;
 
 public class Payment{
-	  protected double amount;
+	protected double amount;
 
-		public Payment(){
-			this.amount=0;
-		}
+	public Payment(){
+		this.amount=0;
+	}
 
-		public Payment(double amount){
+	public Payment(double amount){
+		if(amount>=0) {
 			this.amount=amount;
-		}
-
-		public double getAmount(){
-			return this.amount;	
-		}
-
-		public void setAmount(double amount){
-			this.amount=amount;
-		}
-
-		public String paymentDetails(){
-			return "This amount is "+amount;
 		}
 	}
+
+	public double getAmount(){
+		return this.amount;	
+	}
+
+	public void setAmount(double amount){
+		if(amount >= 0) {
+			this.amount=amount;
+		}
+	}
+
+	public String paymentDetails(){
+		return "This amount is "+amount;
+	}
+}

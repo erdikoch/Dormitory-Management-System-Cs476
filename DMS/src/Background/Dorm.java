a934d3dc8e175dff89621669464cbede2751d2f4
Erdi <erdi.koc@ozu.edu.tr>
2015-03-28 11:09:06 +0200

    Update Dorm.java

--- DMS/src/Background/Dorm.java
+++ DMS/src/Background/Dorm.java
@@ -1,6 +1,7 @@
 package background;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Dorm {
@@ -28,12 +29,18 @@ public class Dorm {
 		return dormSize;
 	}
 	public void setDormSize(int dormSize) {
-		dormSize+=1;
+		dormSize = 500;
+		this.dormSize = dormSize;
 	}
 	public int getFilledDormSize() {
 		return filledDormSize;
 	}
 	public void setFilledDormSize(int filledDormSize) {
+		Random random = new Random();
+		int Low = 100;
+		int High = 500;
+		int R = random.nextInt(High-Low) + Low;
+		filledDormSize = R;
 		this.filledDormSize = filledDormSize;
 	}
 

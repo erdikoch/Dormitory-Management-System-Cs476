0b257380c5e63b65d0d64b1c77f0ee775be0966a
Erdi <erdi.koc@ozu.edu.tr>
2015-03-28 02:33:02 +0200

    Create Payment.java

---
+++ DMS/src/Background/Payment.java
@@ -0,0 +1,25 @@
+package background;
+
+public class Payment{
+	  protected double amount;
+
+		public Payment(){
+			this.amount=0;
+		}
+
+		public Payment(double amount){
+			this.amount=amount;
+		}
+
+		public double getAmount(){
+			return this.amount;	
+		}
+
+		public void setAmount(double amount){
+			this.amount=amount;
+		}
+
+		public String paymentDetails(){
+			return "This amount is "+amount;
+		}
+	}

28c7548f61055690a401342bcfc9529ec56229c8
Mehmet Kayaalp <mehmet.kayaalp@ozu.edu.tr>
2015-03-28 16:54:03 +0200

    TestCreditCardPayment.java and TestPayment.java

---
+++ DMS/src/test/TestCreditCardPayment.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import background.CreditCardPayment;
+
+public class TestCreditCardPayment {
+
+	@Test
+	public void testCreditCardName() {
+		CreditCardPayment creditCardPayment = new CreditCardPayment();
+		creditCardPayment.setcardName("Visa");
+		assertEquals("Visa", creditCardPayment.getCardName());
+	}
+	
+	@Test
+	public void testEmptyCreditCardName() {
+		CreditCardPayment creditCardPayment = new CreditCardPayment();
+		creditCardPayment.setcardName("");
+		assertEquals("", creditCardPayment.getCardName());
+	}
+	
+	@Test
+	public void testBlankCreditCardName() {
+		CreditCardPayment creditCardPayment = new CreditCardPayment();
+		creditCardPayment.setcardName(" ");
+		assertEquals(" ", creditCardPayment.getCardName());
+	}
+	
+	
+
+}

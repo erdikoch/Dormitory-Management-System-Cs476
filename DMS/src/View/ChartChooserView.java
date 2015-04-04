e22cd971f9185a3e660ae6550035150de3b730b6
erdikoch <erdi.koc@ozu.edu.tr>
2015-04-05 02:10:21 +0300

    Chart and viewer changes !

--- DMS/src/View/ChartChooserView.java
+++ DMS/src/View/ChartChooserView.java
@@ -1,4 +1,5 @@
 package view;
+
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -17,9 +18,9 @@ import org.jfree.ui.RefineryUtilities;
 
 import background.Dorm;
 import database.DBConnection;
+import javax.swing.JRadioButton;
 
 public class ChartChooserView extends javax.swing.JFrame {
-	private JCheckBox maleFemaleCapacity;
 	public ChartChooserView() {
 		setTitle("Choose Chart Data Type");
 		GridBagLayout gridBagLayout = new GridBagLayout();
@@ -28,14 +29,14 @@ public class ChartChooserView extends javax.swing.JFrame {
 		gridBagLayout.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
 		getContentPane().setLayout(gridBagLayout);
-		
+
 		JLabel lblDorm = new JLabel("Dorm");
 		GridBagConstraints gbc_lblDorm = new GridBagConstraints();
 		gbc_lblDorm.insets = new Insets(0, 0, 5, 5);
 		gbc_lblDorm.gridx = 1;
 		gbc_lblDorm.gridy = 4;
 		getContentPane().add(lblDorm, gbc_lblDorm);
-		
+
 		final JComboBox<String> comboBox = new JComboBox<String>();
 		DBConnection connection = new DBConnection();
 		ArrayList<String> list;
@@ -48,67 +49,76 @@ public class ChartChooserView extends javax.swing.JFrame {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		
-		
+
+
+
 		GridBagConstraints gbc_comboBox = new GridBagConstraints();
 		gbc_comboBox.insets = new Insets(0, 0, 5, 5);
 		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboBox.gridx = 2;
 		gbc_comboBox.gridy = 4;
 		getContentPane().add(comboBox, gbc_comboBox);
-		
-		
-		
+
+
+
 		JLabel lblChartType = new JLabel("Chart Type");
 		GridBagConstraints gbc_lblChartType = new GridBagConstraints();
 		gbc_lblChartType.insets = new Insets(0, 0, 5, 5);
 		gbc_lblChartType.gridx = 1;
 		gbc_lblChartType.gridy = 6;
 		getContentPane().add(lblChartType, gbc_lblChartType);
-		
+
 		ButtonGroup group = new ButtonGroup();
 		
-		final JCheckBox dormCapacityCheckBox = new JCheckBox("Dorm Capacity");
-		GridBagConstraints gbc_dormCapacityCheckBox = new GridBagConstraints();
-		gbc_dormCapacityCheckBox.insets = new Insets(0, 0, 5, 5);
-		gbc_dormCapacityCheckBox.gridx = 2;
-		gbc_dormCapacityCheckBox.gridy = 6;
-		group.add(dormCapacityCheckBox);
-		getContentPane().add(dormCapacityCheckBox, gbc_dormCapacityCheckBox);
+		final JRadioButton dormCapacityRadioButton = new JRadioButton("Dorm Capacity");
+		GridBagConstraints gbc_dormCapacityRadioButton = new GridBagConstraints();
+		gbc_dormCapacityRadioButton.insets = new Insets(0, 0, 5, 5);
+		gbc_dormCapacityRadioButton.gridx = 2;
+		gbc_dormCapacityRadioButton.gridy = 6;
+		getContentPane().add(dormCapacityRadioButton, gbc_dormCapacityRadioButton);
 		
-		maleFemaleCapacity = new JCheckBox("Male/Female Capacity");
-		GridBagConstraints gbc_maleFemaleCapacity = new GridBagConstraints();
-		gbc_maleFemaleCapacity.gridwidth = 2;
-		gbc_maleFemaleCapacity.insets = new Insets(0, 0, 5, 5);
-		gbc_maleFemaleCapacity.gridx = 2;
-		gbc_maleFemaleCapacity.gridy = 7;
-		group.add(maleFemaleCapacity);
-		getContentPane().add(maleFemaleCapacity, gbc_maleFemaleCapacity);
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
 		gbc_btnNewButton.gridx = 4;
 		gbc_btnNewButton.gridy = 8;
 		JButton btnNewButton = new JButton("Get Chart");
 		
+		final JRadioButton genderRadioButton = new JRadioButton("Gender Rate");
+		GridBagConstraints gbc_genderRadioButton = new GridBagConstraints();
+		gbc_genderRadioButton.anchor = GridBagConstraints.WEST;
+		gbc_genderRadioButton.insets = new Insets(0, 0, 5, 5);
+		gbc_genderRadioButton.gridx = 2;
+		gbc_genderRadioButton.gridy = 7;
+		getContentPane().add(genderRadioButton, gbc_genderRadioButton);
+		getContentPane().add(btnNewButton, gbc_btnNewButton);
+		
+		group.add(dormCapacityRadioButton);
+		group.add(genderRadioButton);
+
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(dormCapacityCheckBox.isSelected()){
-					Dorm dorm =new Dorm();
-					dorm.setDormName(comboBox.getSelectedItem().toString());
-					final CapacityWindow dormCapacity = new CapacityWindow("Dorm Capacity",dorm);
-					dormCapacity.pack();
-			        RefineryUtilities.centerFrameOnScreen(dormCapacity);
-			        dormCapacity.setVisible(true);
-				}else if(maleFemaleCapacity.isSelected()){
+				if(dormCapacityRadioButton.isSelected())
+					try {
+						
+							Dorm dorm = new Dorm();
+							dorm.setDormName(comboBox.getSelectedItem().toString());
+							CapacityWindow dormCapacity = new CapacityWindow(dorm);
+
+						
+					} catch (Exception e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				else if(genderRadioButton.isSelected()){
 					final MaleFemaleCapacityWindow capacity = new MaleFemaleCapacityWindow("Dorm Male/Female Capacity");
 					capacity.pack();
-			        RefineryUtilities.centerFrameOnScreen(capacity);
-			        capacity.setVisible(true);
+					RefineryUtilities.centerFrameOnScreen(capacity);
+					capacity.setVisible(true);
 				}
 			}
 		});
-		getContentPane().add(btnNewButton, gbc_btnNewButton);
+		
+		
 	}
 
 }

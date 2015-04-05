4a58fe281f07fd51ddc46e6ed0800a589b766ef7
erdikoch <erdi.koc@ozu.edu.tr>
2015-04-05 23:13:58 +0300

    Payment Background finished !

--- DMS/src/View/ItemsListWindow.java
+++ DMS/src/View/ItemsListWindow.java
@@ -38,6 +38,8 @@ public class ItemsListWindow extends javax.swing.JFrame {
 	private JMenuItem deletedLostItem, deletedDamagedItem;
 	private JPopupMenu popupMenu, popupMenuForD;
 
+	// private Object[][] lostTableData = new Object[8][8];
+
 	public ItemsListWindow() {
 		initComponents();
 
@@ -60,13 +62,14 @@ public class ItemsListWindow extends javax.swing.JFrame {
 		popupMenu = new JPopupMenu();
 		deletedLostItem = new JMenuItem("Closed");
 		deletedLostItem.addActionListener(new ActionListener() {
-
+			
 			@Override
 			public void actionPerformed(ActionEvent evt) {
-				Object id = lostItemTable.getValueAt(
-						lostItemTable.getSelectedRow(), 0);
 				lostItemTable.getSelectedRow();
-				new CloseLostItemWindow(id).setVisible(true);
+				new RemoveLostItemWindow().setVisible(true);
+				System.out.println(lostItemTable.getSelectedRow());
+				System.out.println(deletedLostItem.getText());
+				
 			}
 		});
 		popupMenu.add(deletedLostItem);
@@ -78,6 +81,7 @@ public class ItemsListWindow extends javax.swing.JFrame {
 			}
 		});
 
+		
 		lostItemTable.setModel(new javax.swing.table.DefaultTableModel(
 				new Object[][] { {}, {}, {}, {} }, new String[] {}));
 
@@ -89,30 +93,16 @@ public class ItemsListWindow extends javax.swing.JFrame {
 				new Color(102, 102, 102), new Color(0, 0, 51)));
 		scrollPane_1.setBounds(498, 37, 246, 224);
 		getContentPane().add(scrollPane_1);
-
+		
 		damagedItemTable = new JTable();
-		damagedItemTable.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mousePressed(MouseEvent evt) {
-				clickOnDmgTable(evt);
-			}
-		});
 		popupMenuForD = new JPopupMenu();
-
 		deletedDamagedItem = new JMenuItem("Closed");
 		deletedDamagedItem.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (!damagedItemTable.getValueAt(
-						damagedItemTable.getSelectedRow(), 2).equals("Closed")) {
-					conn.updateDmgItemStatus(damagedItemTable.getValueAt(
-							damagedItemTable.getSelectedRow(), 0));
-					damagedItemTable.setValueAt("Closed",
-							damagedItemTable.getSelectedRow(), 2);
-				}
+				
 			}
 		});
-
 		popupMenuForD.add(deletedDamagedItem);
 		damagedItemTable.setComponentPopupMenu(popupMenuForD);
 		damagedItemTable.setModel(new javax.swing.table.DefaultTableModel(
@@ -121,14 +111,12 @@ public class ItemsListWindow extends javax.swing.JFrame {
 		scrollPane_1.setViewportView(damagedItemTable);
 
 		JScrollPane scrollPane = new JScrollPane();
-
 		scrollPane.setViewportBorder(new EtchedBorder(EtchedBorder.LOWERED,
 				new Color(102, 102, 102), new Color(0, 0, 51)));
 		scrollPane.setBounds(248, 37, 246, 224);
 		getContentPane().add(scrollPane);
 
 		closedItemTable = new JTable();
-		closedItemTable.setModel(fillClosedTable());
 		scrollPane.setViewportView(closedItemTable);
 
 		JPanel panel = new JPanel();
@@ -174,30 +162,10 @@ public class ItemsListWindow extends javax.swing.JFrame {
 		lostModel.addColumn("Item");
 		lostModel.addColumn("Status");
 		for (int i = 0; i < conn.getLostId().size(); i++) {
-			if (conn.getLostStatus().get(i).equals("In progress")) {
-				lostModel
-						.addRow(new Object[] { conn.getLostId().get(i),
-								conn.getLostName().get(i),
-								conn.getLostStatus().get(i) });
-			}
+			lostModel.addRow(new Object[] { conn.getLostId().get(i),
+					conn.getLostName().get(i), conn.getLostStatus().get(i) });
 		}
 		return lostModel;
-	}
-
-	private TableModel fillClosedTable() {
-		DefaultTableModel closedModel = new DefaultTableModel();
-		closedModel.addColumn("Item ID");
-		closedModel.addColumn("Item");
-		closedModel.addColumn("Status");
-		for (int i = 0; i < conn.getLostId().size(); i++) {
-			if (conn.getLostStatus().get(i).equals("Closed")) {
-				closedModel
-						.addRow(new Object[] { conn.getLostId().get(i),
-								conn.getLostName().get(i),
-								conn.getLostStatus().get(i) });
-			}
-		}
-		return closedModel;
 	}
 
 	private TableModel fillDamagedTable() {
@@ -220,13 +188,6 @@ public class ItemsListWindow extends javax.swing.JFrame {
 		}
 		if (lostItemTable.getSelectedColumn() == 0 && evt.getClickCount() == 2) {
 			System.out.println(lostItemTable.getSelectedRow());
-		}
-	}
-
-	private void clickOnDmgTable(MouseEvent evt) {
-		if (damagedItemTable.getSelectedColumn() == 0
-				&& evt.getClickCount() == 1) {
-			System.out.println("napalim napalim");
 		}
 
 	}

a56d8cc036f1f7355156f965d365ab7e931ece40
Erdi <erdi.koc@ozu.edu.tr>
2015-04-13 13:06:07 +0300

    Update Dorm.java
    Updated according to the testCases

--- DMS/src/Background/Dorm.java
+++ DMS/src/Background/Dorm.java
@@ -1,8 +1,21 @@
 package background;
 
+import java.sql.SQLException;
+
+import database.DBConnection;
+
 public class Dorm {
 	private String dormName;
 	private String location;
+	private Dorm dorm;
+
+	public Dorm(Dorm dorm) {
+		this.dorm = dorm;
+	}
+
+	public Dorm() {
+
+	}
 
 	public String getDormName() {
 		return dormName;
@@ -22,6 +35,18 @@ public class Dorm {
 		if (!location.equals("") || !location.equals(" ")) {
 			this.location = location;
 		}
+	}
+
+	public int getMaleNumber() throws SQLException {
+		DBConnection connection = new DBConnection();
+		connection.connect();
+		return connection.getMaleNumber(dorm);
+	}
+
+	public Object getFemaleNumber() throws SQLException {
+		DBConnection connection = new DBConnection();
+		connection.connect();
+		return connection.getFemaleNumber(dorm);
 	}
 
 }
